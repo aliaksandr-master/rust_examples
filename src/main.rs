aceaede22e185ab473e7db501ef6d606bfbaa1bb
master <alxe.master@gmail.com>
2020-05-17 15:12:34 +0300

    add test example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn test_cell() {
+    use std::cell::Cell;
+
+    let a = Cell::new(123);
+
+    assert_eq!(a.get(), 123);
+    a.set(444);
+    assert_eq!(a.get(), 444);
+    assert_eq!(a.get(), 444);
+}
